3741789304b972b3e2aa9e88ab9d3f27b8e2b632
Aaryan-0s <aaryanbasnet16@gmail.com>
2021-09-29 12:55:48 +0545

    initials of game

---
+++ game_initial.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+import random
+
+
+screen = pygame.display.set_mode((400, 700))
+pygame.display.set_caption("Flappybird")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
+
+# For background image
+bg_surface = pygame.image.load('background-day.png').convert()
+bg_surface = pygame.transform.scale(bg_surface, (400, 700))
+
+def game_loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.blit(bg_surface, (0, 0))
+        pygame.display.update()
+        clock.tick(60)
+
+game_loop()
